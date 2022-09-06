58c539ac3c1a3f8b1aa2c76d72b717f8e596998c
lac0220 <nemeth0220@gmail.com>
2022-09-06 23:04:14 +0200

    Add JS loader animation

--- dist/js/script.js
+++ dist/js/script.js
@@ -1,8 +1,20 @@
 /******/ (() => { // webpackBootstrap
+/******/ 	"use strict";
 var __webpack_exports__ = {};
 /*!**************************!*\
   !*** ./src/js/script.js ***!
   \**************************/
 
+//loader-animation
+window.addEventListener("load", () => {
+    const loader = document.querySelector(".loader-animation");
+
+    loader.classList.add("loader-animation-hidden");
+
+    loader.addEventListener("transform", () => {
+        document.body.removeChild("loader");
+    })
+}) 
 /******/ })()
 ;
+//# sourceMappingURL=script.js.map
---
+++ src/js/script.js
@@ -0,0 +1,11 @@
+"use strict";
+//loader-animation
+window.addEventListener("load", () => {
+    const loader = document.querySelector(".loader-animation");
+
+    loader.classList.add("loader-animation-hidden");
+
+    loader.addEventListener("transform", () => {
+        document.body.removeChild("loader");
+    })
+}) 
